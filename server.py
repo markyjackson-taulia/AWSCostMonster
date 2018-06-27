c179d37d049165323cd4edf0c505ea58063b5342
Grant <zukeru@gmail.com>
2018-06-26 19:59:22 -0700

    initial commit of AWS COST Monster, currently not intended for use.

--- server.py
+++ server.py
@@ -6,10 +6,11 @@ import calendar
 from dateutil.relativedelta import relativedelta
 import collections
 
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 def get_aws_data(first_date, current_date):
-
     client = boto3.client('ce')
-
     Dimensions = client.get_dimension_values(
         TimePeriod={
             'Start': first_date,
@@ -18,9 +19,7 @@ def get_aws_data(first_date, current_date):
         Dimension='SERVICE',
         Context='COST_AND_USAGE'
     )
-
     cost_dict = {}
-
     for dimension in Dimensions["DimensionValues"]:
         dimension_to_search = str(dimension["Value"])
         response = client.get_cost_and_usage(
@@ -47,7 +46,6 @@ def get_aws_data(first_date, current_date):
                 },
             ]
         )
-
         for result in response["ResultsByTime"]:
             for cost in result["Groups"]:
                 if cost["Metrics"]["BlendedCost"]["Amount"] > 0 or cost["Metrics"]["UnblendedCost"]["Amount"] > 0:
@@ -66,181 +64,121 @@ def get_aws_data(first_date, current_date):
     return cost_dict
 
 def generate_months(months):
-
     dates_dict = collections.OrderedDict()
     starting_and_end_days = []
     months = int(months)
-
     for month_count in range(months,0,-1):
         six_months = datetime.datetime.today() + relativedelta(months=-month_count)
         last_day_of_month = calendar.monthrange(six_months.year, six_months.month)[1]
         six_month = six_months.month
-
         if six_month != 10 and six_month != 11 and six_month != 12:
             six_month = "0" + str(six_month)
-
         sixth_months_first = str(six_months.year) + "-" + str(six_month) + "-" + "01"
         sixth_months_last = str(six_months.year) + "-" + str(six_month) + "-" + str(last_day_of_month)
         starting_and_end_days.append((sixth_months_first, sixth_months_last))
-
     months_dict = collections.OrderedDict()
     last_month_total = 0
     months_list = []
-
     for first_day, last_day in starting_and_end_days:
-
         old_month = int(last_day.split("-")[1]) - 1
         if len(str(old_month)) < 2:
             old_month = "0" + str(old_month)
-
         one_month_forward = int(last_day.split("-")[1]) + 1
-
         if len(str(one_month_forward)) < 2:
             one_month_forward = "0" + str(one_month_forward)
-
         year = last_day.split("-")[0]
-
         if str(one_month_forward) == "01":
             year = int(year) + 1
             year = str(year)
         last_day = year + "-" + one_month_forward + "-01"
-
         cost_dict = get_aws_data(first_day, last_day)
-
         total_cost = 0.0
-
         time_frame = str(first_day) + ":" + str(last_day)
         months_list.append(first_day)
     return months_list
 
 def generate_monthly_cost_report(months):
-
     dates_dict = collections.OrderedDict()
     starting_and_end_days = []
     months = int(months)
-
     for month_count in range(months,0,-1):
         six_months = datetime.datetime.today() + relativedelta(months=-month_count)
         last_day_of_month = calendar.monthrange(six_months.year, six_months.month)[1]
         six_month = six_months.month
-
         if six_month != 10 and six_month != 11 and six_month != 12:
             six_month = "0" + str(six_month)
-
         sixth_months_first = str(six_months.year) + "-" + str(six_month) + "-" + "01"
         sixth_months_last = str(six_months.year) + "-" + str(six_month) + "-" + str(last_day_of_month)
         starting_and_end_days.append((sixth_months_first, sixth_months_last))
-
     months_dict = collections.OrderedDict()
     last_month_total = 0
     for first_day, last_day in starting_and_end_days:
-
         old_month = int(last_day.split("-")[1]) - 1
         if len(str(old_month)) < 2:
             old_month = "0" + str(old_month)
-
         one_month_forward = int(last_day.split("-")[1]) + 1
-
         if len(str(one_month_forward)) < 2:
             one_month_forward = "0" + str(one_month_forward)
-
         year = last_day.split("-")[0]
-
         if str(one_month_forward) == "01":
             year = int(year) + 1
             year = str(year)
         last_day = year + "-" + one_month_forward + "-01"
-
         cost_dict = get_aws_data(first_day, last_day)
-
         total_cost = 0.0
-
         time_frame = str(first_day) + ":" + str(last_day)
         if time_frame not in months_dict.keys():
             months_dict[time_frame] = {}
-
         for cost in cost_dict:
             if cost_dict[cost]["UnblendedCost"] >= 0.1:
                 total_cost = cost_dict[cost]["UnblendedCost"] + total_cost
                 last_month_total = total_cost
-
         dates_dict[first_day] = total_cost
         days = int(str(last_day).split("-")[2]) - int(str(first_day).split("-")[2]) + 1
         average_by_day = total_cost / float(days)
-
         months_dict[time_frame]["cost"] = {}
         months_dict[time_frame]["cost"] = cost_dict
-
         months_dict[time_frame]["total"] = {}
         months_dict[time_frame]["total"] = total_cost
-
         months_dict[time_frame]["average"] = {}
         months_dict[time_frame]["average"] = str(average_by_day)
-
-
     return dates_dict, months_dict, round(last_month_total, 2)
 
 def generate_month_cost_report():
-
     todays_date = datetime.datetime.now()
     day = todays_date.day
     year = todays_date.year
     month = todays_date.month
     first_day = "01"
-
     if month != "10" and month != "11" and month != "12":
         first_date = str(year) + "-0" + str(month) + "-" + str(first_day)
-
         if len(str(day)) < 2:
             day = "0" + str(day)
-
         current_date = str(year) + "-0" + str(month) + "-" + str(day)
     else:
         first_date = str(year) + "-" + str(month) + "-" + str(first_day)
-
         if len(str(day)) < 2:
             day = "0" + str(day)
-
         current_date = str(year) + "-" + str(month) + "-" + str(day)
-
     cost_dict = get_aws_data(first_date, current_date)
-
     time_frame = str(first_date) + ":" + str(current_date)
-
     total_cost = 0.0
-
     for cost in cost_dict:
         if cost_dict[cost]["UnblendedCost"] >= 0.1:
             total_cost = cost_dict[cost]["UnblendedCost"] + total_cost
-
     average_by_day = total_cost / float(day)
-
     last_day_of_month = calendar.monthrange(datetime.datetime.now().year, datetime.datetime.now().month)[1]
-
     number_of_days_left_in_month = last_day_of_month - datetime.datetime.now().day
-
     total_left_for_month = average_by_day * number_of_days_left_in_month
-
     return cost_dict, total_left_for_month, total_cost, first_date, time_frame, average_by_day
-
-app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/api/getmonthtotalcost/<months>')
 @cross_origin()
 def getmonthtotalcost(months):
     number_of_months = months
-    cost_dict, total_left_for_month, total_cost, first_date, time_frame_this_month, average_by_day = generate_month_cost_report()
-
-    predicted_total_cost = total_cost + total_left_for_month
-    predicted_total_cost = round(predicted_total_cost, 2)
-    predicted_total_cost_string = " $" + str(predicted_total_cost)
-
     number_of_months = str(number_of_months).strip()
     number_of_months = int(number_of_months)
     dates_dict, months_dict, last_month_total = generate_monthly_cost_report(number_of_months)
-    dates_dict[first_date] = predicted_total_cost
-
     months_total = []
     for item in months_dict:
         month_total = months_dict[item]["total"]
@@ -261,17 +199,9 @@ def generatecost():
 @cross_origin()
 def totalsbymonth(months):
     number_of_months = months
-    cost_dict, total_left_for_month, total_cost, first_date, time_frame_this_month, average_by_day = generate_month_cost_report()
-
-    predicted_total_cost = total_cost + total_left_for_month
-    predicted_total_cost = round(predicted_total_cost, 2)
-    predicted_total_cost_string = " $" + str(predicted_total_cost)
-
     number_of_months = str(number_of_months).strip()
     number_of_months = int(number_of_months)
     dates_dict, months_dict, last_month_total = generate_monthly_cost_report(number_of_months)
-    dates_dict[first_date] = predicted_total_cost
-
     months_total = []
     count = -1
     for item in months_dict:
@@ -295,11 +225,8 @@ def costpredictions(months):
     cost_predictions_dict["Predicted Remaining Monthly Cost"] = round(total_left_for_month,2)
     cost_predictions_dict["Average Cost by Day"] = round(average_by_day,2)
     cost_predictions_dict["Current Total Cost"] = round(total_cost,2)
-    print total_left_for_month
-    print first_date
     cost_predictions_dict["Predicted Total Cost"] = round(total_cost, 2) + round(total_left_for_month,2)
     return jsonify(cost_predictions_dict)
-
 
 @app.route('/api/months/<months>')
 @cross_origin()

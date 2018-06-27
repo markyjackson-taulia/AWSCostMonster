0b5c0a743162527e7626050c560ffeffd4f38947
Grant <zukeru@gmail.com>
2018-06-26 19:53:09 -0700

    initial commit of AWS COST Monster, currently not intended for use.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AWSCostMonster
 AWS Cost Monster is opensource AWS Cost Analysis and Predictions.
+
+## This is the initial commit, This is just an alpha not intended for use.
---
+++ script.js
@@ -0,0 +1,136 @@
+var projected_costs = [1012,2071,3200,4336,5493,6743,8612,10581,13169,15857,18545,21333];
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+var month_costs = httpGet("http://127.0.0.1:5000/api/getmonthtotalcost/5");
+var months_cost = JSON.parse(month_costs);
+var totals_by_month_costs = httpGet("http://127.0.0.1:5000/api/totalsbymonth/5");
+var totals_by_month_cost = JSON.parse(totals_by_month_costs);
+
+months = ["January 1 2018","Febuary 1 2018","March 1 2018","April 1 2018","May 1 2018","June 1 2018","July 1 2018","August 1 2018","September 1 2018","October 1 2018","November 1 2018","December 1 2018"];
+
+var ctx = document.getElementById("chartjs-intro-myChart");
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: months,
+    datasets: [
+      { 
+        data: months_cost,
+        label: "Actual Monthly Cost",
+        borderColor: "#3e95cd",
+        fill: true
+      },
+      {
+        data: totals_by_month_cost,
+        label: "Total by Month",
+        borderColor: "#d9f441",
+        fill: true
+      },
+      {
+        data: projected_costs,
+        label: "Projected Totals by Month",
+        borderColor: "#f44242",
+        fill: true
+      }
+    ]
+  }
+});
+
+var ctx = document.getElementById("chartjs-intro-donought");
+var cost_by_service = httpGet("http://127.0.0.1:5000/api/getcostby_service");
+var cost_by_service_list = JSON.parse(cost_by_service);
+
+var service_names = [];
+var service_costs = [];
+
+for (var i in cost_by_service_list) {
+  var service_name = cost_by_service_list[i][0];
+  var service_cost = cost_by_service_list[i][1];
+  service_names.push(service_name);
+  service_costs.push(service_cost);
+}
+
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+      data: {
+        labels: service_names,
+        datasets: [
+          {
+            data: service_costs,
+            label: "Cost By Service",
+            borderColor: "#3e95cd",
+            backgroundColor: [
+                "#FF6384",
+                "#d9f441",
+                "#3e95cd",
+                "#8463FF",
+                "#6384FF",
+                "#f44242",
+                "#3e95cd",
+                "#d9f441",
+                "#63FF84",
+                "#1a8e0b",
+                "#072784",
+                "#02b59a",
+                "#2a03b5",
+                "#02b59a",
+                "#67b502",
+                "#b75d03",
+                "#2a03b5"
+            ],
+            fill: true
+          }
+        ]
+      }
+});
+
+var ctx = document.getElementById("chartjs-intro-bar");
+var cost_by_service = httpGet("http://127.0.0.1:5000/api/costpredictions/5");
+var cost_by_service_object = JSON.parse(cost_by_service);
+var labels = [];
+var costs = [];
+
+for (var i in cost_by_service_object) {
+  labels.push(i);
+  costs.push(cost_by_service_object[i]);
+}
+
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            data: costs,
+            label: "Cost Predictions",
+            borderColor: "#3e95cd",
+            backgroundColor: [
+                "#FF6384",
+                "#d9f441",
+                "#3e95cd",
+                "#8463FF",
+                "#6384FF",
+                "#f44242",
+                "#3e95cd",
+                "#d9f441",
+                "#63FF84",
+                "#1a8e0b",
+                "#072784",
+                "#2a03b5",
+                "#2a03b5",
+                "#02b59a",
+                "#67b502",
+                "#b75d03",
+                "#02b59a"
+            ],
+            fill: true
+          }
+        ]
+      }
+});
